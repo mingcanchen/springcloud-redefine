61670401a95c2d74316b02598b42200cc532d983
mingcanchen <mingcanchen@hotmail.com>
2018-11-12 18:10:36 +0800

    ribbon

---
+++ ch5-2/client-a/src/main/java/com/ming/clienta/ClientAApplication.java
@@ -0,0 +1,12 @@
+package com.ming.clienta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAApplication.class, args);
+	}
+}
---
+++ ch5-2/ribbon-loadbalancer/src/main/java/com/ming/ribbonloadbalancer/RibbonLoadbalancerApplication.java
@@ -0,0 +1,12 @@
+package com.ming.ribbonloadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonLoadbalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonLoadbalancerApplication.class, args);
+    }
+}
