aa60add79c40b016a084766995920d91dbda3f03
mingcanchen <mingcanchen@hotmail.com>
2018-11-28 18:01:12 +0800

    zuul高级配置

---
+++ ch8-7/zuul-server/src/main/java/com/ming/zuulserver/filter/HeaderDeliverFilter.java
@@ -0,0 +1,38 @@
+package com.ming.zuulserver.filter;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.*;
+
+import org.springframework.context.annotation.Configuration;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ * @author chenmingcan
+ */
+@Configuration
+public class HeaderDeliverFilter extends ZuulFilter {
+
+	@Override
+	public String filterType() {
+		return PRE_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return PRE_DECORATION_FILTER_ORDER + 1;// =6
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext context = RequestContext.getCurrentContext();
+		context.addZuulRequestHeader("result", "to next service");
+		return null;
+	}
+}
---
+++ ch8-7/zuul-server/src/main/java/com/ming/zuulserver/filter/ModifyRequestEntityFilter.java
@@ -0,0 +1,55 @@
+package com.ming.zuulserver.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_DECORATION_FILTER_ORDER;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * @author chenmingcan
+ */
+@Configuration
+public class ModifyRequestEntityFilter extends ZuulFilter {
+
+	@Override
+	public String filterType() {
+		return PRE_TYPE;
+	}
+
+	@Override
+	public int filterOrder() {
+		return PRE_DECORATION_FILTER_ORDER + 1;// =6
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		request.getParameterMap();
+		Map<String, List<String>> requestQueryParams = ctx.getRequestQueryParams();
+		if (requestQueryParams == null){
+			requestQueryParams = new HashMap<>();
+		}
+		//这里添加新增参数的value，注意，只取list的0位
+		ArrayList<String> arrayList = new ArrayList<>();
+		arrayList.add("1wwww");
+		requestQueryParams.put("test", arrayList);
+		ctx.setRequestQueryParams(requestQueryParams);
+		return null;
+	}
+}
