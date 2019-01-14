6c4f2cd57fab3ecea0409289c145a75df1ed2477
mingcanchen <mingcanchen@hotmail.com>
2019-01-14 18:00:25 +0800

    gateway综合应用

--- ch18-1/provider/src/main/java/com/ming/provider/ProviderApplication.java
+++ ch18-1/provider/src/main/java/com/ming/provider/ProviderApplication.java
@@ -2,9 +2,12 @@ package com.ming.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author chenmingcan
+ * @EnableDiscoveryClient 从Spring Cloud Edgware开始，@EnableDiscoveryClient 或@EnableEurekaClient 可省略。
+ * 只需加上相关依赖，并进行相应配置，即可将微服务注册到服务发现组件上。
  */
 @SpringBootApplication
 public class ProviderApplication {
