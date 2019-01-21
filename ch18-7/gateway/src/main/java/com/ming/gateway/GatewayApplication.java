1ef5a03797afc3e1a11c30c950c4451877d32339
mingcanchen <mingcanchen@hotmail.com>
2019-01-21 18:20:41 +0800

    gateway动态

---
+++ ch18-7/gateway/src/main/java/com/ming/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.ming.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenmingcan
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
+
